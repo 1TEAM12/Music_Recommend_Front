a66046b73611c439be843edc02626377e8488f97
Jay <jvidorca@gmail.com>
2022-11-07 17:53:15 +0900

    Fix : 검색, 추천 후 상세페이지 이동 기능 수정
    -1. search_api.js, song_rcm_api.js, song_detail.js
    -2. 특정 노래 선택 후 상세페이지 이동 기능 구현

--- static/script/song_rcm_api.js
+++ static/script/song_rcm_api.js
@@ -1,5 +1,4 @@
 $(songrecommend)
-
 const songId = location.href.split('=')[1]
 
 //노래 GET
@@ -30,19 +29,21 @@ async function loadSongsTitle(){
     const song_detail = await SongDetailView(songId)
 
     const p_result_title = document.getElementById("result_title")
-        
+
     p_result_title.innerHTML =  `<p style="text-align:center;font-size:30px;font-weight: bold;">선택된 노래 "${song_detail.title}"</p>`
 }
 
 loadSongsTitle()
 
+
 async function rcmParam(){
     let getLink = window.location.search;
-    let getLink_Name = getLink.split('?');
+    let getLink_Name = getLink.split('=');
     let getLink_result = getLink_Name[1]
     let decodeResult = decodeURI(getLink_result);
     console.log(decodeResult);
 }
+
 
 async function songrecommend(){
     let getLink = window.location.search;
@@ -52,9 +53,6 @@ async function songrecommend(){
     console.log(decodeResult);
 
 
-
-
-    
     const response = await fetch(`http://127.0.0.1:8000/songs/${decodeResult}/recommend/`, {
         method: 'POST',
         headers: {
@@ -71,7 +69,7 @@ async function songrecommend(){
     response_json.forEach(item => {
         $('#rcm-box').append(
             `<figure style="display: inline-block;margin:10px;">
-            <a href="#" onclick="move_detail_page(${item.id})">
+            <a href="#" onclick="move_detail_page(${item.pk}+1)">
                 <div class="img-wrapper">
                     <img src="${item.image}" style="width:300px;height:300px;"onerror="this.src='https://avatars.githubusercontent.com/u/114125954?s=200&v=4'">
                     <div class="img-overlay text-white text-center">
@@ -94,4 +92,9 @@ async function songrecommend(){
         </figure>`
         )
     });
+}
+
+function move_detail_page(click_id){ 
+    console.log(click_id)
+    window.location.href = `/song_detail.html?$id=${click_id}`;
 }
